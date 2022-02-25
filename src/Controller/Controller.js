743c4fa979deac5b90221f764c2509d54ae2d445
Filipe Voges <filipe.vogesh@gmail.com>
2022-02-25 00:56:00 -0300

    Bot FrameWork

---
+++ src/Controller/Controller.js
@@ -0,0 +1,16 @@
+'use strict';
+
+/**
+ * Controller Class
+ *
+ * @method constructor
+ */
+module.exports = class Controller {
+
+    /**
+     * Run the Bot
+     */
+    static run() {
+        //TODO: Code Here
+    }
+}
